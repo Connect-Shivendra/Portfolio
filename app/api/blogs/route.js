a2e12fd3276dabe2a99d5666e9909ad2672fd921
Connect-Shivendra <connect.shivendra@gmail.com>
2025-05-18 21:29:21 +1000

    remove console logs

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -3,10 +3,7 @@ import { getAllBlogs } from '@/app/utils/mdx-utils';
 
 export async function GET() {
   try {
-    console.log('API: Fetching all blogs');
     const blogs = getAllBlogs();
-    console.log(`API: Found ${blogs.length} blogs`);
-    console.log('API: Blog categories:', blogs.map(blog => blog.frontmatter.category));
     return NextResponse.json({ blogs });
   } catch (error) {
     console.error('Error fetching blogs:', error);
